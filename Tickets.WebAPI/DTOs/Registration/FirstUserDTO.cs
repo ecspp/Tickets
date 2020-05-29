3ae935583baf60b16f6f62015153f56894ce99d2
Everton C. da Silva <everton.silva@bling.com.br>
2020-05-29 19:36:16 -0300

    Remove projeto de DataAnnotations

--- Tickets.WebAPI/DTOs/Registration/FirstUserDTO.cs
+++ Tickets.WebAPI/DTOs/Registration/FirstUserDTO.cs
@@ -1,13 +1,8 @@
 
-
-using Tickets.DataAnnotations;
-
 namespace Tickets.WebAPI.DTOs
 {
     public class FirstUserDTO
-    {
-        [UserNameAttribute]
-        public string UserName { get; set; }
+    {        public string UserName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
     }
