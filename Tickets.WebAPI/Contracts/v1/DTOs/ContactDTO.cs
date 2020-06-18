f9511f8e4785d88eafd3c8d70d904091bb7122c4
Everton C. da Silva <everton.rsc@gmail.com>
2020-06-18 19:00:31 -0300

    Remove companyId do contatodto

--- Tickets.WebAPI/Contracts/v1/DTOs/ContactDTO.cs
+++ Tickets.WebAPI/Contracts/v1/DTOs/ContactDTO.cs
@@ -10,7 +10,6 @@ namespace Tickets.WebAPI.Contracts.v1.DTOs
         public int? Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-        public int? CompanyId { get; set; }
         public ICollection<ContactTypeDTO> ContactTypes { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
